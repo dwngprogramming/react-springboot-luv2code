7c7f1dd9a3de8d517eb30e37adda1a4e7fea6862
dwngprogramming <dwnq.coding@gmail.com>
2024-02-21 10:39:24 +0700

    React Day 1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
 import './App.css';
+import TodoTable from "./component/TodoTable";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const todos = [
+        {rowNumber: 1, rowDescription: "Feed dog", rowAssigned: "user1"},
+        {rowNumber: 2, rowDescription: "Water plant", rowAssigned: "user2"},
+        {rowNumber: 3, rowDescription: "Make dinner", rowAssigned: "user1"},
+        {rowNumber: 4, rowDescription: "Charge phone battery", rowAssigned: "user1"},
+    ];
+
+    const addTodo = () => {
+        if (todos.length > 0) {
+            const newTodo = {
+                rowNumber: todos.length + 1,
+                rowDescription: "New Todo",
+                rowAssigned: "User 3"
+            };
+            todos.push(newTodo);
+            console.log(todos);
+        }
+    }
+
+    return (
+        <div className={"mt-5 container"}>
+            <div className={"card"}>
+                <div className={"card-header"}>
+                    Your To do
+                </div>
+                <div className={"card-body"}>
+                    <TodoTable todos={todos}/>
+                    <button className={"btn btn-primary"} onClick={addTodo}>
+                        Add new todos
+                    </button>
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default App;
